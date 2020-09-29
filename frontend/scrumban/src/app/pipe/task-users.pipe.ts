bc77ba8a4a8739e89284034c944d6e4c3f2477c9
hubigabi <hubigabi19@gmail.com>
2020-09-29 03:40:33 +0200

    Added Assign and Dismiss button for task

--- frontend/scrumban/src/app/pipe/task-users.pipe.ts
+++ frontend/scrumban/src/app/pipe/task-users.pipe.ts
@@ -6,21 +6,25 @@ import {User} from '../model/user.model';
 })
 export class TaskUsersPipe implements PipeTransform {
 
-  counter = 0;
-
-  transform(users: User[], args?: any): any {
-    return this.getUsersByTask(users);
+  transform(users: User[], loggedUser?: User, args?: any): any {
+    if (loggedUser) {
+      return this.isLoggedUserInTaskUsers(users, loggedUser);
+    } else {
+      return this.getUsersByTask(users);
+    }
   }
 
   getUsersByTask(users: User[]) {
-    console.log('--------------------------------------------------');
-
     if (users.length === 0) {
       return '';
     }
     return users.map(value => value.name)
       .sort((a, b) => a.localeCompare(b))
       .join(', ');
+  }
+
+  isLoggedUserInTaskUsers(users: User[], loggedUser: User) {
+    return users.map(value => value.id).indexOf(loggedUser.id) > -1;
   }
 
 }
