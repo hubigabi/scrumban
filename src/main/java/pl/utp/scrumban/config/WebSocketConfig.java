1cd02b553bc0f1056ffeb8c5ec9cbe9989d02d30
hubigabi <hubigabi19@gmail.com>
2020-10-06 15:16:50 +0200

    Fixed: Added web socket for project

--- src/main/java/pl/utp/scrumban/config/WebSocketConfig.java
+++ src/main/java/pl/utp/scrumban/config/WebSocketConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/task")
+        registry.addEndpoint("/scrumban")
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/updatedTask");
+        registry.enableSimpleBroker("/updateTask", "/updateProject");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
--- src/main/java/pl/utp/scrumban/controller/WebSocketController.java
+++ src/main/java/pl/utp/scrumban/controller/WebSocketController.java
@@ -6,7 +6,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import pl.utp.scrumban.model.Project;
 import pl.utp.scrumban.model.Task;
+import pl.utp.scrumban.service.ProjectService;
 import pl.utp.scrumban.service.TaskService;
 
 @RestController
@@ -14,16 +16,24 @@ import pl.utp.scrumban.service.TaskService;
 public class WebSocketController {
 
     private TaskService taskService;
+    private ProjectService projectService;
 
     @Autowired
-    public WebSocketController(TaskService taskService) {
+    public WebSocketController(TaskService taskService, ProjectService projectService) {
         this.taskService = taskService;
+        this.projectService = projectService;
     }
 
     @MessageMapping("/task/{project_id}")
-    @SendTo("/updatedTask/{project_id}")
+    @SendTo("/updateTask/{project_id}")
     public Task updateTask(@DestinationVariable String project_id, Task task) {
         return taskService.updateTask(task);
+    }
+
+    @MessageMapping("/project/{project_id}")
+    @SendTo("/updateProject/{project_id}")
+    public Project updateProject(@DestinationVariable String project_id, Project project) {
+        return projectService.updateProject(project);
     }
 
 }
