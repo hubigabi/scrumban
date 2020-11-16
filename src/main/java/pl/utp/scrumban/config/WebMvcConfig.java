9947a834b628f9b3456961709a44886dd35d4afd
hubigabi <hubigabi19@gmail.com>
2020-11-16 14:14:24 +0100

    Added Swagger to registry

--- src/main/java/pl/utp/scrumban/config/WebMvcConfig.java
+++ src/main/java/pl/utp/scrumban/config/WebMvcConfig.java
@@ -14,6 +14,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
         registry.addResourceHandler("/**/*")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
