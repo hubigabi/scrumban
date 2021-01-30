fc875c68cda45c1f838a938cd143c836a08d3960
hubigabi <hubigabi19@gmail.com>
2021-01-30 23:56:35 +0100

    Added Angular build

--- frontend/scrumban/src/environments/environment.prod.ts
+++ frontend/scrumban/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://scrumban1.herokuapp.com'
+  baseUrl: 'http://localhost:8080'
+  // baseUrl: 'https://scrumban1.herokuapp.com'
 };
