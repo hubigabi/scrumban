149bb6fb0d07c98187f5b41e3844c07fefb481ae
hubigabi <hubigabi19@gmail.com>
2021-12-13 17:27:27 +0100

    Fixed tests by using H2

--- src/test/java/pl/utp/scrumban/ScrumbanApplicationTests.java
+++ src/test/java/pl/utp/scrumban/ScrumbanApplicationTests.java
@@ -2,8 +2,10 @@ package pl.utp.scrumban;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class ScrumbanApplicationTests {
 
 	@Test
