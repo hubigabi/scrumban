a0252e6ca65daa3bc28794ee56a94d59b1d02765
hubigabi <hubigabi19@gmail.com>
2022-02-06 00:09:05 +0100

    Build for Heroku

--- frontend/scrumban/src/environments/environment.ts
+++ frontend/scrumban/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080'
-  // baseUrl: 'https://scrumban1.herokuapp.com'
+  // baseUrl: 'http://localhost:8080'
+  baseUrl: 'https://scrumban1.herokuapp.com'
 };
 
 /*
